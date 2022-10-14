3e4bd8c26539285f30c9dd2960c6c95e8c019e1a
computernerdzach <wizteczach@gmail.com>
2022-10-14 10:56:41 -0700

    design

--- gui.py
+++ gui.py
@@ -4,6 +4,14 @@ from random import randint as r
 from words import words as w
 
 
+def on_enter(e):
+    btn.configure(bg="lightblue")
+
+
+def on_leave(e):
+    btn.configure(bg="beige")
+
+
 def get_words(words):
     index1 = r(0, (len(words) - 1))
     index2 = r(0, (len(words) - 1))
@@ -21,17 +29,20 @@ def button_click():
 window = Tk()
 window.title("Two Words")
 window.geometry('560x325')
+window.configure(bg="steelblue")
 
 lbl1 = Label(window, text="TWO", font='Times 14', width=30, height=15, bg="lightblue", padx=1, pady=1)
 lbl1.grid(column=0, row=0)
 
-lblSpace = Label(window, width=1, height=17, bg="lightgrey")
+lblSpace = Label(window, width=1, height=17, bg="steelblue")
 lblSpace.grid(column=1, row=0)
 
 lbl2 = Label(window, text="WORDS", font='Times 14', width=30, height=15, bg="lightblue", padx=1, pady=1)
 lbl2.grid(column=2, row=0)
 
-btn = Button(window, text="TWO WORDS NOW!", command=button_click, width=30)
+btn = Button(window, text="TWO WORDS NOW!", command=button_click, width=20, bg="beige", border=0, )
 btn.grid(column=0, row=100, columnspan=3)
+btn.bind("<Enter>", on_enter)
+btn.bind("<Leave>", on_leave)
 
 window.mainloop()
